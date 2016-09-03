14a6a2b02bc250d76cd9355787cd0726bb92bd4f
Alexandru.Sabou <sabou.alexandru2540@gmail.com>
2016-09-03 10:11:09 +0300

    Fixed getMyCards

--- player.js
+++ player.js
@@ -7,15 +7,16 @@ module.exports = {
     
     console.log(game_state);
       
-    var cards =  getCards(game_state);
+    var cards =  getMyCards(game_state);
      
     if (cards[0].rank === cards[1].rank) {
        bet(game_state.pot)
-       return;
+    } else {
+       bet(game_state.current_buy_in);          
     }
       
       
-    bet(game_state.current_buy_in);  
+    
   },
 
   showdown: function(game_state) {
