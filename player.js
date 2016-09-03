8bbff9ef373d6b995f5fdb02b399e913a4f2e031
Alexandru.Sabou <sabou.alexandru2540@gmail.com>
2016-09-03 13:54:04 +0300

    Handle post blind

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    VERSION: "Default JavaScript folding player",
+    VERSION: "Please JS, please STAPHHH",
 
     bet_request: function(game_state, bet) {
 
@@ -29,7 +29,18 @@ module.exports = {
                 y = trans[cards[1].rank],
                 c1 = cards[0].suit,
                 c2 = cards[1].suit,
-                cardsDown = game_state.community_cards;
+                cardsDown = game_state.community_cards,
+                ranks = {
+                    "NIMNIC": 0,
+                    "PAIR": 1,
+                    "TWO_PAIRS": 2,
+                    "THREE_OF_A_KIND": 3,
+                    "STRAIGHT": 4,
+                    "FLUSH": 5,
+                    "FULL_HOUSE": 6,
+                    "FOUR_OF_A_KIND": 7,
+                    "ROYAL_FLUSH": 8
+                };
 
             // OPENING HAND
             // The cards are equal
@@ -42,7 +53,7 @@ module.exports = {
                 } else if (c2 === c1) {
                     myBet = game_state.current_buy_in;
                     if (Math.abs(x - y) < 4) {
-                        myBet =  bid(game_state.current_buy_in, player.stack / 4);
+                        myBet = bid(game_state.current_buy_in, player.stack / 4);
                     }
                 }
                 if (Math.abs(x - y) < 4) {
@@ -51,7 +62,37 @@ module.exports = {
 
             } else if (cardsDown.length === 3) {
                 if (myBet > 0) {
-                    myBet = game_state.current_buy_in;
+                    var rank = getRanks(cardsDown, hand, ranks);
+                    myBet = bid(game_state.current_buy_in, player.stack / 4);
+                    switch (rank) {
+                        case 8:
+                            myBet = player.stack;
+                            break;
+                        case 7:
+                            myBet = player.stack;
+                            break;
+                        case 6:
+                            myBet = player.stack;
+                            break;
+                        case 5:
+                            myBet = player.stack;
+                            break;
+                        case 4:
+                            myBet = player.stack;
+                            break;
+                        case 3:
+                            myBet = player.stack;
+                            break;
+                        case 2:
+                            myBet = bid(game_state.current_buy_in, player.stack / 3);
+                            break;
+                        case 1:
+                            myBet = bid(game_state.current_buy_in, player.stack / 5);
+                            break;
+                        default:
+                            myBet = 0;
+                    }
+
                 }
             }
 
@@ -91,9 +132,86 @@ module.exports = {
                 }
             }
         }
-        
+
         function bid(call, possible) {
             return possible < call ? call : possible;
+        }
+
+        function getRanks(cardsDown, hand, ranks) {
+            var cards = cardsDown.concat(hand),
+                cardsDownNr = cardsDown.length,
+                numbers = {
+                    "1": 0,
+                    "2": 0,
+                    "3": 0,
+                    "4": 0,
+                    "5": 0,
+                    "6": 0,
+                    "7": 0,
+                    "8": 0,
+                    "9": 0,
+                    "10": 0,
+                    "J": 0,
+                    "Q": 0,
+                    "K": 0,
+                    "A": 0,
+                },
+                color = {
+                    "diamonds": 0,
+                    "spades": 0,
+                    "hearts": 0,
+                    "clubs": 0
+                },
+                card = null;
+
+            for (var i = 0; i < cardsDownNr; i++) {
+                card = cards[i];
+                numbers[card.rank] = numbers[card.rank] + 1;
+                color[card.suit] = color[card.suit] + 1;
+            }
+
+            for (var key in numbers) {
+                if (numbers[key] == 4) {
+                    return ranks["FOUR_OF_A_KIND"];
+                }
+            }
+
+            for (var key in color) {
+                if (color[key] > 4) {
+                    return ranks["FLUSH"];
+                }
+            }
+
+            var threeOfAKind = false;
+            for (var key in numbers) {
+                if (numbers[key] == 3) {
+                    threeOfAKind = true;
+                }
+            }
+
+            for (var key in numbers) {
+                var pairs = 0;
+                if (numbers[key] > 2) {
+                    pairs++;
+                }
+
+                if (threeOfAKind) {
+                    if (pairs > 1) {
+                        return ranks["FULL_HOUSE"];
+                    }
+
+                    return ranks["THREE_OF_A_KIND"];
+                }
+
+                if (pairs == 1) {
+                    return ranks["PAIRS"];
+                }
+
+                if (pairs > 1) {
+                    return ranks["TWO_PAIRS"];
+                }
+            }
+            return 0;
         }
 
     },
