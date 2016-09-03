e3e70c853fbcc9972c6d99b6850be875c505b860
varaghar <Jozsef.Kerekes@isdcnet.local>
2016-09-03 10:07:06 +0300

    added getcard

--- player.js
+++ player.js
@@ -24,16 +24,36 @@ module.exports = {
   },
     
  getMyCards: function(game_state) {
-   
-      var cards_down = game_state.community_cards;
-      var players = game_state.players;
-        
+    var players = game_state.players;
      var nrOfPlayers = players.size;
         for (var i=0; i<nrOfPlayers;i++) {
             if (player.name == 'Comfortable Pugs') {
                 return player.hole_cards;
             }
         }
-     
+    },
+    getAllCards: function(game_state) {
+        var myCards = getMyCards(game_state);
+        var cards_down = game_state.community_cards;
+        
+        cards.push(myCards[0]);
+        cards.push(myCards[1]);
+        
+        var cards = [];
+        var cardsDownSize = cards_down.size; 
+         for (var i=0; i<cardsDownSize;i++) {
+            cards.push(cards_down[i]);
+        }
+        return cards;
+    },
+    getMyPlayer: function(game_state) {
+              var players = game_state.players;
+        
+     var nrOfPlayers = players.size;
+        for (var i=0; i<nrOfPlayers;i++) {
+            if (player.name == 'Comfortable Pugs') {
+                return player;
+            }
+        }
     }
 };
